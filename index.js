f5b520b1a94ded03ac0b48ca8456c0f3c311e8d7
leo-ngra <leonardo.ngra@gmail.com>
2023-01-24 09:13:22 -0300

    Inserindo regex e captura dos links

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import fs from 'fs';	
 import chalk from 'chalk';
 
+ function extraiLinks(texto) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capturas = [...texto.matchAll(regex)]
+    const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
+
+    return resultados
+ }
+
 function trataErro(erro) {
     console.log(erro)
     throw new Error(chalk.red(erro.code, 'Não há arquivo no diretório.'));
@@ -11,20 +19,10 @@ async function pegaArquivo(caminhoDoArquivo) {
     try{
         const encolding = 'utf-8'
         const texto = await fs.promises.readFile(caminhoDoArquivo, encolding)
-        console.log(chalk.green(texto))
+        console.log(extraiLinks(texto))
     } catch(erro) {
         trataErro(erro)
     }
 }
 
-// Promises com then()
-/* function pegaArquivo(caminhoDoArquivo) {
-    const encolding = 'utf-8'
-    fs.promises
-        .readFile(caminhoDoArquivo, encolding)
-        .then((texto) => console.log(chalk.green(texto)))
-        .catch((erro) => trataErro(erro))
-} */
-
 pegaArquivo('./arquivos/texto.md')
-pegaArquivo('./arquivos/')
